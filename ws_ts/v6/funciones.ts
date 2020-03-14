d548491e4ed6325c08b3c901731c01c029d7700d
Rubby Casallas <rcasalla@uniandes.edu.co>
2020-03-14 12:15:43 -0500

    mas funciones mas funciones

--- ws_ts/v6/funciones.ts
+++ ws_ts/v6/funciones.ts
@@ -14,3 +14,28 @@ function imprimir(aImprimir : string) : void {
 
 imprimir1();
 imprimir('Otra vez Hola mundo');
+
+function sumar(op1: number, op2:number) : number{
+return op1 + op2;
+}
+
+console.log(sumar(3,4));
+
+let varRestar : any = function restar(op1: number, op2:number) : number{
+    return op1 - op2;
+    };
+
+console.log(varRestar(4,3));
+console.log(varRestar);
+
+let varMult = (op1:number,op2:number) => op1*op2;
+ 
+console.log(varMult(10,2));
+
+console.log(varMult);
+
+function calculadora(op1:number, op2:number, operador:any) : number {
+    return operador(op1,op2);
+}
+ 
+console.log(calculadora(10,2,varMult));
